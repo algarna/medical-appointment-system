e0c150a249cf5bdbf875385ef8ee02980a72c6ed
Alberto Garcia <111655060+algarna@users.noreply.github.com>
2026-05-06 08:48:40 +0200

    test: configure Testcontainers for integration tests

--- patient-service/src/test/java/com/medapp/patient_service/PatientServiceApplicationTests.java
+++ patient-service/src/test/java/com/medapp/patient_service/PatientServiceApplicationTests.java
@@ -2,12 +2,20 @@ package com.medapp.patient_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest
+@Testcontainers
 class PatientServiceApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
 	@Test
 	void contextLoads() {
 	}
-
 }
